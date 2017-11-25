41714ec49cc511798e12f4f89f916320f1836071
roberto rhodes <roberto_rhodes@outlook.com>
2017-11-25 15:54:38 +0000

    started styling with css

--- app/components/NavigationBar/NavigationBar.js
+++ app/components/NavigationBar/NavigationBar.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import styles from './NavigationBar.css';
 
 export class NavigationBar extends React.Component{
 
     render(){
 
-       return <button onClick={this.props.onClick}> DO NOT CLICK </button>;
+       return   (
+                <div className="navigationBarContainer">
+                <div  className="Bio" onClick={this.props.onClick}> Bio </div>
+                <div  className="Projects" onClick={this.props.onClick}> Projects </div>
+                <div  className="CV" onClick={this.props.onClick}> CV </div>
+                </div>
+                );
     }
     
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 var HTMLWebpackPlugin = require('html-webpack-plugin');
 var HTMLWebpackPluginConfig = new 
 HTMLWebpackPlugin({
-    template: __dirname + '/WebsiteHeader/index.html',
+    template: __dirname + '/app/index.html',
     filename: 'index.html',
     inject: 'body'
 });
 
 
 module.exports ={
-    entry: __dirname + '/WebsiteHeader/index.js',
+    entry: __dirname + '/app/index.js',
     module:{
         loaders: [
             {
